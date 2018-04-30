ff55c36c6d974e32a29d50b1781f20fb2583fc74
Carlo Reyes <creyes@mirman.org>
2018-04-30 15:51:10 -0700

    making functions

--- choice.sh
+++ choice.sh
@@ -44,7 +44,109 @@ set_dock_user () {
 
 }
 
-PS3='Main Choices: 1:Printers 2:MSC Manifests 3:Rename Laptop 4:Add/Remove Users 5:Enable/Disable Securly 6:Update MSC 7:Set Dock 8:Quit '
+#Rename machine
+rename_machine () {
+    echo "Renaming this machine. Please use approved naming convention."
+    read -p "What is the username? : " rename
+    
+    sudo scutil --set HostName $rename
+    sudo scutil --set LocalHostName $rename
+    sudo scutil --set ComputerName $rename
+    
+    echo "Laptop successfully renamed to $rename! Please restart terminal to see changes."
+    echo ""
+    echo "Changes are listed below:"
+    echo "HostName: $(sudo scutil --get HostName)"
+    echo "LocalHostName: $(sudo scutil --get LocalHostName)"
+    echo "ComputerName: $(sudo scutil --get ComputerName)"
+    echo ""  
+}
+
+#Main Options
+main_option () {
+    PS3='Main Choices: 1:Printers 2:MSC Manifests 3:Rename Laptop 4:Add/Remove Users 5:Enable/Disable Securly 6:Update MSC 7:Set Dock 8:Quit '  
+}
+
+#Edit MSC Client Identifier
+msc_identity () {
+    sudo defaults write /Library/Preferences/ManagedInstalls.plist ClientIdentifier $1
+}
+
+#Adding a user
+add_user () {
+    read -p "What is the username of the user?: " ausername
+    read -p "What is the real name of the user?: " arealname
+    read -p "What is the password for this user?: " apassword
+
+    usernum=$(dscl . -list /Users UniqueID | sort -nr -k 2 | head -1 | grep -oE '[0-9]+$')
+    usernum=$((usernum+1))
+        while true; do
+            read -p "Is this user an Administrator for this laptop? [Y or N]: " adminchoice
+            case $adminchoice in
+                [Yy] ) echo "$ausername is set to Administrator."
+                        aprimarygroupid=80
+                break;;
+                [Nn] ) echo "$ausername is set to Standard."
+                        aprimarygroupid=20
+                break;;
+                * ) echo "Please select Y or N for Admin question."
+            esac
+        done
+
+    sudo dscl . -create /Users/$ausername
+    sudo dscl . -create /Users/$ausername UserShell /bin/bash
+    sudo dscl . -create /Users/$ausername RealName "$arealname"
+    sudo dscl . -create /Users/$ausername UniqueID $usernum
+    sudo dscl . -create /Users/$ausername PrimaryGroupID $aprimarygroupid
+    sudo dscl . -create /Users/$ausername NFSHomeDirectory /Users/$ausername
+    sudo dscl . -passwd /Users/$ausername "$apassword"
+    sudo dscl . -append /Groups/staff GroupMembership $ausername 
+    sudo dscl . delete /Users/$ausername jpegphoto
+    sudo dscl . create /Users/$ausername Picture /Library/User\ Pictures/Nature/Earth.png
+    
+    sudo createhomedir -c 2>&1 | grep -v "shell-init"
+    sudo dseditgroup -o edit -a $ausername -t user _lpadmin
+
+    echo "Creating user $ausername."
+    echo "Creating real name $arealname."
+    echo "Creating password of $apassword"
+    echo "Creating Unique ID of $usernum."
+    echo "$ausername can now add printers."
+}
+
+#List users allowed to be removed.
+list_users () {
+    echo "Users List:"
+    echo "$(dscl . -list /Users | grep -v '_' | grep -v 'daemon' | grep -v 'root' | grep -v 'nobody' | grep -v '/' | grep -v 'remote')"
+}
+
+#Delete a user
+delete_user () {
+    read -p "What is the username to be removed?: " userdelete
+    while true; do 
+		read -p "You picked $userdelete. Is this right? [Y or N]" deletechoice
+            case $deletechoice in 
+                [Yy] )
+                    echo "Checking if user exists..."
+                    if [ -e /Users/$userdelete ]; then
+                        echo "User $userdelete found! Removing..."
+                        
+                        sudo dseditgroup -o edit -d $userdelete -t user _lpadmin
+                        sudo dscl . delete /Users/$userdelete
+                        sudo rm -rf /Users/$userdelete
+                    else
+                        echo "User not found! Please try again."
+                    fi
+                break;;
+                [Nn] ) 
+                    echo "OK."
+                break;;
+                * ) echo "Please answer Y or N. ";;
+            esac
+    done
+}
+
+main_option
 options=("Printers" "MSC Manifest" "Rename Laptop" "Add/Remove Users" "Enable/Disable Securly" "Update MSC" "Set Dock" "Quit")
 select opt in "${options[@]}"
 do
@@ -93,18 +195,18 @@ do
                             case $manifestopt in
                                 "student_us")
                                     echo "Changing manifest to $manifestopt"
-                                    sudo defaults write /Library/Preferences/ManagedInstalls.plist ClientIdentifier $manifestopt
-                                    PS3='Main Choices: 1:Printers 2:MSC Manifests 3:Rename Laptop 4:Add/Remove Users 5:Enable/Disable Securly 6:Update MSC 7:Set Dock 8:Quit '
+                                    msc_identity $manifestopt
+                                    main_option
                                     break;;
                                 "student_ls")
                                     echo "Changing manifest to $manifestopt"
-                                    sudo defaults write /Library/Preferences/ManagedInstalls.plist ClientIdentifier $manifestopt
-                                    PS3='Main Choices: 1:Printers 2:MSC Manifests 3:Rename Laptop 4:Add/Remove Users 5:Enable/Disable Securly 6:Update MSC 7:Set Dock 8:Quit '
+                                    msc_identity $manifestopt
+                                    main_option
                                     break;;
                                 "faculty")
                                     echo "Changing manifest to $manifestopt"
-                                    sudo defaults write /Library/Preferences/ManagedInstalls.plist ClientIdentifier $manifestopt
-                                    PS3='Main Choices: 1:Printers 2:MSC Manifests 3:Rename Laptop 4:Add/Remove Users 5:Enable/Disable Securly 6:Update MSC 7:Set Dock 8:Quit '
+                                    msc_identity $manifestopt
+                                    main_option
                                     break;;
                                 *) echo "Choose a valid manifest number."
                             esac
@@ -117,18 +219,7 @@ do
                 done
             ;;
         "Rename Laptop")
-            echo "Renaming this machine. Please use approved naming convention."
-            read -p "What is the username? : " rename
-            sudo scutil --set HostName $rename
-            sudo scutil --set LocalHostName $rename
-            sudo scutil --set ComputerName $rename
-            echo "Laptop successfully renamed to $rename! Please restart terminal to see changes."
-            echo ""
-            echo "Changes are listed below:"
-            echo "HostName: $(sudo scutil --get HostName)"
-            echo "LocalHostName: $(sudo scutil --get LocalHostName)"
-            echo "ComputerName: $(sudo scutil --get ComputerName)"
-            echo ""
+            rename_machine
             ;;
         "Add/Remove Users")
             echo "Adding / Removing Users"
@@ -136,65 +227,13 @@ do
                 read -p "Are you trying to (A)dd or (R)emove a user?: " addremovechoice
                     case $addremovechoice in
                     [Aa] )
-                        read -p "What is the username of the user?: " ausername
-                        read -p "What is the real name of the user?: " arealname
-                        read -p "What is the password for this user?: " apassword
-
-                        usernum=$(dscl . -list /Users UniqueID | sort -nr -k 2 | head -1 | grep -oE '[0-9]+$')
-                        usernum=$((usernum+1))
-                            while true; do
-                            read -p "Is this user an Administrator for this laptop? [Y or N]: " adminchoice
-                                case $adminchoice in
-                                [Yy] ) echo "$ausername is set to Administrator."
-                                aprimarygroupid=80
-                                break;;
-                                [Nn] ) echo "$ausername is set to Standard."
-                                aprimarygroupid=20
-                                break;;
-                                * ) echo "Please select Y or N for Admin question."
-                                esac
-                            done
-                        sudo dscl . -create /Users/$ausername
-                        sudo dscl . -create /Users/$ausername UserShell /bin/bash
-                        sudo dscl . -create /Users/$ausername RealName "$arealname"
-                        sudo dscl . -create /Users/$ausername UniqueID $usernum
-                        sudo dscl . -create /Users/$ausername PrimaryGroupID $aprimarygroupid
-                        sudo dscl . -create /Users/$ausername NFSHomeDirectory /Users/$ausername
-                        sudo dscl . -passwd /Users/$ausername "$apassword"
-                        sudo dscl . -append /Groups/staff GroupMembership $ausername 
-                        sudo dscl . delete /Users/$ausername jpegphoto
-                        sudo dscl . create /Users/$ausername Picture /Library/User\ Pictures/Nature/Earth.png
-                        
-                        sudo createhomedir -c 2>&1 | grep -v "shell-init"
-
-                        echo "Creating user $ausername."
-                        echo "Creating real name $arealname."
-                        echo "Creating password of $apassword"
-                        echo "Creating Unique ID of $usernum."
+                        add_user
                         restart_function
                         break;;
                     [Rr] ) 
-                        echo "Users List:"
-                        echo "$(dscl . -list /Users | grep -v '_' | grep -v 'daemon' | grep -v 'root' | grep -v 'nobody')"
-                        read -p "What is the username to be removed?: " userdelete
-                                while true; do 
-		                            read -p "You picked $userdelete. Is this right? [Y or N]" deletechoice
-                                    case $deletechoice in 
-                                        [Yy] )
-                                            echo "Checking if user exists..."
-                                            if [ -e /Users/$userdelete ]; then
-                                            echo "User $userdelete found! Removing..."
-                                            sudo dscl . delete /Users/$userdelete
-                                            sudo rm -rf /Users/$userdelete
-                                            else
-                                            echo "User not found! Please try again."
-                                            fi
-                                            break;;
-                                        [Nn] ) echo "OK.";break;;
-                                        * ) echo "Please answer Y or N. ";;
-                                    esac
-                                done
-                                break;;
+                        list_users
+                        delete_user
+                        break;;
                     * )
                         echo "Please use A or R."
                     esac
